9e3749ace9e09959a3d2130624eb80994b086557
HWZ <30350209+hanwzhang@users.noreply.github.com>
2023-01-26 16:11:26 -0500

    demo version 1.26

--- clipmaster4_vanilla.py
+++ clipmaster4_vanilla.py
@@ -1,7 +1,10 @@
 # libraries
+print('Welcome. Loading...')
 from bs4 import BeautifulSoup
 from bs4.element import Comment
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import re
 import pandas as pd
 # dictionaries - rules for text extraction
@@ -62,26 +65,26 @@ def tag_content(link, element): # testing if a line is part of the text
         else: judge = True
     return judge
 
-def format_author(byLine):
+def format_author(link,byLine):
     auth = byLine.get_text().strip()
-    # nyp multiple authors workaround
-    stringL = auth.split('\n')
-    removeWords = ['Social Links','Author','author','required','Submit','Δ','follow']
-    for x in stringL: 
-        if any(y in x for y in removeWords): stringL.remove(x)
-    auth = re.sub('\s+',' ',''.join(stringL))
-    auth = re.sub(' \S*@\S*','', auth)
-    # end of nyp multiple authors workaround
+    if get_site_info(link)[0] == 'POLITICO': auth = re.sub('\s+',' ',re.sub(' Close.*?\n.*?bio page', '', auth)).replace(' ,',',') # political multiple authors workaround
+    if get_site_info(link)[0] == 'NY POST': # nyp multiple authors workaround
+        stringL = auth.split('\n')
+        removeWords = ['Social Links','Author','author','required','Submit','Δ','follow']
+        for x in stringL: 
+            if any(y in x for y in removeWords): stringL.remove(x)
+        auth = re.sub('\s+',' ',''.join(stringL))
+        auth = re.sub(' \S*@\S*','', auth) # end of nyp multiple authors workaround
     for x in ['|','New York Daily News','Updated','Published', 'Police']: auth = auth.split(x)[0]
-    for y in ['By ','By','by ']:
+    for y in ['By ','By','by ',': ']:
         if auth.startswith(y) == True: auth = auth.replace(y, '', 1)
     return auth
 
 def generate_info(link, soup): # create formatted title & site & author & link
-    try: title = soup.find(get_site_info(link)[4]).string.split(' - ')[0].strip()
+    try: title = soup.find(get_site_info(link)[4]).string.split(' - ')[0].split(' | ')[-1].strip()
     except: title = ''
     if get_site_info(link)[7] >= 0: title = 'Opinion: ' + title
-    try: auth = format_author(soup.select(get_site_info(link)[3][0])[get_site_info(link)[3][1]])
+    try: auth = format_author(link,soup.select(get_site_info(link)[3][0])[get_site_info(link)[3][1]])
     except: auth = ''
     info = title +'\n'+ get_site_info(link)[0] +' - '+ auth +'\n'+ link
     return info
@@ -116,24 +119,36 @@ def generate_emailSubject(link, text):
     else: subject = subjectShort + '[MEA] ' + title
     return subject
 
-url = input('Paste URL here:')
-driver = webdriver.Chrome()
-driver.get(url)
-result_soup = BeautifulSoup(driver.page_source, 'html.parser')
-#driver.close()
-text = generate_info(url, result_soup) + text_from_html(url, result_soup)
-print('▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬CLIP STARTS HERE▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n')
-print(generate_emailSubject(url, text) + text)
-try: print(get_site_info(url)[8])
-except: print('\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬CLIP SUCCESSFUL!▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬')
+cont = True
+options = webdriver.ChromeOptions()
+options.add_argument("--window-size=720,1080")
+while cont == True:
+    url = input('Paste URL here:')
+    #driver = webdriver.Chrome("chromedriver.exe", options=options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+    try:
+        driver.get(url)
+        print('\nLoading clip......\n')
+        result_soup = BeautifulSoup(driver.page_source, 'html.parser')
+        driver.close()
+        text = generate_info(url, result_soup) + text_from_html(url, result_soup)
+        print('▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬CLIP STARTS HERE▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n')
+        print(generate_emailSubject(url, text) + text)
+        try: print(get_site_info(url)[8])
+        except: print('\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬CLIP SUCCESSFUL!▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬')
+    except:
+        driver.close() 
+        print('Error: Link format not recognized')
+    contmsg = input('Keep clipping? y/n')
+    if contmsg == 'y':
+        cont = True
+    else: cont = False
+print('Thank you for using Clipmaster.')
 
 # Issues:
 
 # STREETSBLOG cannot remove embedded twitter - but now it's an independent paragraph
-# opinion detector does not support NYP, becuz they don't differentiate it in the link
 
-# When running inside a code editor like MS visual code, writing the clip to a txt is cleaner:
-#f = open('clip.txt', 'w', encoding="utf-8")
-#f.write(generate_emailSubject(url, text) + text)
+# opinion detector does not support NYP, becuz they don't differentiate it in the link
 
 # NYT opinion author's bio is not included because it's separated from main text by 'follow us on twitter' messages
