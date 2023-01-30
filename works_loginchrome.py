a2529a47a150355122227359fabb37dab51a0e32
HWZ <30350209+hanwzhang@users.noreply.github.com>
2023-01-30 11:20:40 -0500

    Delete works_loginchrome.py

--- works_loginchrome.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-options = webdriver.ChromeOptions()
-options.add_argument('user-data-dir=C:/Users/HaZhang/Desktop/clipmaster-main/Clipperprofile') 
-driver = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=options)
-driver.get('https://nytimes.com')
-
-# basically we need to set up a user profile for this app, and copy the whole file into here
-# get it from C:/Users/HaZhang/AppData/Local/Google/Chrome/User Data
